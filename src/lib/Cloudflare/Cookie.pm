4d693ca68e1e9369654ebf124a45dc4200666f6b
Michael Vu <email@michael.vu>
2023-01-01 09:12:35 +0000

    Change method from post to get in Cloudflare::Cookie module

--- src/lib/Cloudflare/Cookie.pm
+++ src/lib/Cloudflare/Cookie.pm
@@ -2,9 +2,10 @@ package Cloudflare::Cookie;
 
 use Dancer2 appname => 'Web';
 
-post qr/.+/ => sub {
-    params->{__cf_chl_tk} or pass;
-    redirect request->path;
+post qr/.*/ => sub {
+    my $params = params;
+    $params->{__cf_chl_tk} or pass;
+    forward request->path, $params, {method => 'GET'};
 };
 
 1;
